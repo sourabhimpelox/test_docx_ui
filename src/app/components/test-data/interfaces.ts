1fc4ca1f261c2c292d55dc09faf89b983cdd94ed
sourabhimpelox <sourabh.jadhav@impelox.com>
2024-12-23 01:22:28 +0530

    tables and styling completed

---
+++ src/app/components/test-data/interfaces.ts
@@ -0,0 +1,42 @@
+export interface PremiumDetail {
+  group_details: string;
+  tob_header: string;
+  tob_value: string;
+}
+
+export interface Category {
+  category_name: string;
+  premium_details: PremiumDetail[];
+}
+
+export interface CensusCategory {
+  category_name: string;
+  census: {}[];
+}
+
+export interface Exclusion {
+  heading: string;
+  bulletPoints: string[];
+  title: string;
+}
+
+export interface EmirateData {
+  emirates: string;
+  exclusions: Exclusion[];
+}
+
+export interface PdfAgeBandDetail {
+  age: string;
+  maternityCount: number;
+  maternityGrossPremium: number;
+  maternityTotalGrossPremium: number;
+  Employee: any;  // Replace with the correct type for Employee
+  Dependents: any;  // Replace with the correct type for Dependents
+}
+
+
+export interface agebandData {
+  category_name: string;
+  pdfAgeBandDetails: PdfAgeBandDetail[];
+}
+
